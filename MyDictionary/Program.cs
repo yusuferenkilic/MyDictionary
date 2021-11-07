3c91cb6f7b38e0ee66dac9a66dd6c712e6870b74
yusuferenkilic <yusuf@DESKTOP-L8N8OGD>
2021-11-07 13:13:50 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("Yusuf");
+            isimler.Print();
+            Console.WriteLine(isimler.Count);
+
+            isimler.Add("İbrahim");
+            isimler.Add("Hasan");
+            isimler.Print();
+            Console.WriteLine(isimler.Count);
+
+        }
+    }
+}
